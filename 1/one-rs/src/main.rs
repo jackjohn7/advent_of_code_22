c5c6302ad905580f94a227fe2fff5c890db244f4
jack <branchjohnchristopher@gmail.com>
2023-11-28 19:44:10 -0600

    moved input, improved 1pt1, and solved 1pt2

--- 1/one-rs/src/main.rs
+++ 1/one-rs/src/main.rs
@@ -1,26 +1,44 @@
-use std::fs::File;
-use std::io::Read;
-
 fn main() {
+    // this macro actually bakes the content of a file into a string at compile time
+    let contents = include_str!("../../input.txt");
 
-    let mut file = File::open("input.txt").unwrap();
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    let results: Vec<u32> = contents
-        .trim()
+    // after having looked at someone else's solution briefly, I learned about .lines, .sum, and .max which greatly simplify what I was doing.
+    //  after learning about those, I rewrote the function using them and it's basically identical to what the guy online wrote.
+    let best_result = contents
         .split("\n\n")
-        .map(|sub_str| sub_str.split("\n")
-        .fold(0 as u32, |acc, x| acc + x.parse::<u32>().unwrap()))
-        .collect();
+        .map(|sub_str| {
+            sub_str
+                .lines()
+                .map(|x| x.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap();
 
-    let mut elf_with_most_food = 0;
-    for (idx, res) in results.iter().enumerate() {
-        if res > &results[elf_with_most_food] {
-            elf_with_most_food = idx;
-        }
-    }
+    println!("The highest calorie count is: {}", best_result);
 
-    println!("The elf with the most food is elf {} with {} cals", elf_with_most_food+1, results[elf_with_most_food]);
+    let result = contents
+        .split("\n\n")
+        .map(|sub_str| {
+            sub_str
+                .lines()
+                .fold(0 as u32, |acc, x| acc + x.parse::<u32>().unwrap())
+        })
+        .enumerate()
+        .fold(
+            None,
+            |acc_max: Option<(usize, u32)>, (idx, cals)| match &acc_max {
+                None => Some((idx + 1, cals)),
+                Some(current_max) => match current_max.1.cmp(&cals) {
+                    std::cmp::Ordering::Less => Some((idx + 1, cals)),
+                    std::cmp::Ordering::Greater | std::cmp::Ordering::Equal => acc_max,
+                },
+            },
+        )
+        .unwrap();
+
+    println!(
+        "The elf with the most food is elf {} with {} cals",
+        result.0, result.1
+    );
 }
