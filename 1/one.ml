e7c26a75887bd2d11287e033d45b8c62ca74bdbe
jack <branchjohnchristopher@gmail.com>
2023-11-28 20:14:42 -0600

    added some stuff to ocaml solution but I know it sucks

--- 1/one.ml
+++ 1/one.ml
@@ -3,25 +3,39 @@ open Printf
 (* Not even remotely finished *)
 
 let data =
-  "1000
-    2000
-    3000
+"1000
+2000
+3000
 
-    4000
+4000
 
-    5000
-    6000
+5000
+6000
 
-    7000
-    8000
-    9000
+7000
+8000
+9000
 
-    10000";;
+10000";;
+
+let max l =
+  let rec aux lst max =
+    match lst with
+    | [] -> max
+    | h :: tl -> if h >= max then aux tl h else aux tl max
+  in
+  aux l 0;;
+
+let sum l =
+  let rec aux lst acc =
+    match lst with
+    | [] -> acc
+    | h :: tl -> aux tl (acc + h)
+  in aux l 0;;
 
 let print_list lst =
-  List.iter (fun x -> print_endline (String.trim x)) lst
+  List.iter (fun x -> print_endline (String.trim x)) lst;;
 
 let () =
-  print_list (String.split_on_char '\n' data);
-
-  print_endline "Hello, world";;
+  print_endline (string_of_int (sum [4; 5; 1;]));
+  print_list (String.split_on_char '\n' data);;
